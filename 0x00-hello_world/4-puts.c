cdddd96a3065082640972414c7626f8acdc63f8a
Tech-sis <abilawonadeola@gmail.com>
2022-03-10 09:53:13 -0800

    write a c program with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * return - Always 0 (success)
+ * return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
