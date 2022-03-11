4582ee0b8fec6a78ef8374d4415058a9ce75421d
Tech-sis <abilawonadeola@gmail.com>
2022-03-11 06:33:27 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[52] = 'abcdefghijklmnopqrstuvwxyz';
+	int i;
+
+	while (alp == i)
+	{
+		putchar(alp[i]);
+	}
+	return (0);
+}
