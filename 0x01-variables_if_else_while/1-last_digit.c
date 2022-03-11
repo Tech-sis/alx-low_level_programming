67657d91f87ebfea73af1d1deecc5b0feb0fff03
Tech-sis <abilawonadeola@gmail.com>
2022-03-11 05:18:34 -0800

    use double variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,24 @@
  */
 int main(void)
 {
-	int n;
+	int n, l;
 
 	srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
+	l = n % 10; 
 	/* your code goes there */
 	if (n > 5)
 	{
-		printf("Last digit of %d is and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	else
 	{
-		printf("Last digit is %d is and is less than 6 and not 0\n", n);
+		printf("Last digit is %d is %d and is less than 6 and not 0\n", n, l);
 	}
 	return (0);
 }
