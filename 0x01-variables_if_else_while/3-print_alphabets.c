495c9dd37ab3a4b731346ffc50ec6a7205da3eda
Tech-sis <abilawonadeola@gmail.com>
2022-03-11 06:56:43 -0800

    print lower and uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,9 @@ int main(void)
 {
 	char alp;
 
-	for (alp = 'a' && 'A'; alp <= 'z' && 'Z'; alp++)
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
 		putchar(alp);
 	putchar('\n');
 	return (0);
