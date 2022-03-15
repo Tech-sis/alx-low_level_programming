921b54128fe2e21ff8c187669ad667517d9b9b11
Tech-sis <abilawonadeola@gmail.com>
2022-03-15 10:28:57 -0700

    prints number

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_to_98 - print numbers in ascending or descending order
+ * @n: gets number
+ * Return: void
+ */
 void print_to_98(int n)
 {
 	while (n < 98)
