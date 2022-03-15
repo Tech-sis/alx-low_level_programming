652351e174b029968985bb87033517214aebb104
Tech-sis <abilawonadeola@gmail.com>
2022-03-15 08:20:02 -0700

    print characters according to inputs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - print character according to input
  * @n: check for number
  * Return: 0, 1 of -1
- */ 
+ */
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,7 +13,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
