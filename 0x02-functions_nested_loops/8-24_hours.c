e7cae971fde4db43d990ad01df9b755adf73471a
Tech-sis <abilawonadeola@gmail.com>
2022-03-15 09:12:19 -0700

    print 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24 hours
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	float i;
+
+	for (i = 00.00; i <= 23.59; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
