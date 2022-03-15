1ba8c53e80e5464316a24ad2cf8b5fd473c273c9
Tech-sis <abilawonadeola@gmail.com>
2022-03-15 13:50:38 +0000

    return according to case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check for lower case letters
+ *
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	int c = 97;
+
+	if (c <= 122)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
