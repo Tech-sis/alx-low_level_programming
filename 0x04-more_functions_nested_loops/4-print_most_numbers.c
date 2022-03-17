eacb4d164bebba03e694f43a987b26bda8fcb9e5
Tech-sis <abilawonadeola@gmail.com>
2022-03-17 06:47:37 -0700

    print some numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print some numbers
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+		if (c == 2 || c == 4)
+		{
+			break;
+		}
+		_putchar('0' + c);
+		c++;
+	}
+}
