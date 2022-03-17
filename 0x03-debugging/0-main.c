685327e106b213beb9a7a9f7ecc7355bd2cf8a67
Tech-sis <abilawonadeola@gmail.com>
2022-03-17 02:05:27 -0700

    test for positive or negative interger

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * main - tests function for positive or negative number
- *
+ * main - tests function that prints if interger is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
