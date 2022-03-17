6681d528438947df71553541070df82748670390
Tech-sis <abilawonadeola@gmail.com>
2022-03-17 05:59:18 -0700

    multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 numbers
+ * @a: takes an integer
+ * @c: takes another integer
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	_putchar(a * b);
+	return (0);
+}
