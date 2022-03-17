4f1ca72c746dc2eefee69be96c490d350bdb14cb
Tech-sis <abilawonadeola@gmail.com>
2022-03-17 07:02:02 -0700

    add numbers 10 itmes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 10 times
+ * @c: check for number
+ * @i: check for number
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 0;
+		while (c <= 14)
+		{
+			_putchar('0' + c);
+				c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
