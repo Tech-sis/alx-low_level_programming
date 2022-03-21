097a527f08420773f3b5d35d6fbbb3dab5350eae
Tech-sis <abilawonadeola@gmail.com>
2022-03-21 16:21:48 -0700

    remove whitespace

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@ void rev_string(char *s)
 {
 	int count = 0, i, j;
 	char *str, temp;
-	
+
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
@@ -17,7 +17,7 @@ void rev_string(char *s)
 		count++;
 	}
 	str = s;
-	
+
 	for (i = 0; i < (count - 1); i++)
 	{
 		for (j = i + 1; j > 0; j--)
