a45c98bda81528846286c4b10473c75ca0ee68ab
Tech-sis <abilawonadeola@gmail.com>
2022-03-23 03:44:35 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ **_strcat - concateates two strings
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	_putchar('*dest + *src');
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+_putchar(char);
+char *_strcat(char *dest, char *src);
