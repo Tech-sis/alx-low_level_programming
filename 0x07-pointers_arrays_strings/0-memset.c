34099d51fe197342984ba004b1128ad224f247e1
Tech-sis <abilawonadeola@gmail.com>
2022-03-28 06:34:32 -0700

    function that returns a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
  * *_memset - fills memeroty with a constant byte
- * @*s: pointer to a variable
+ * @s: pointer to a variable
  * @b: constant byte
  * @n: maximum byte
+ * Return: pointer to constant byte
  */
 
 char *_memset(char *s, char b, unsigned int n)
