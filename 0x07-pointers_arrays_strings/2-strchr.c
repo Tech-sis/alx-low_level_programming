71530e2c2456ff53dddbdd62f22803255e4db90e
Tech-sis <abilawonadeola@gmail.com>
2022-03-28 08:33:43 -0700

    print first occurence of character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: points to a string
+ * @c: points to a character
+ * Return: null or first occurence
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
