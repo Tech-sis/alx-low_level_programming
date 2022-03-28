a65ed6e1c76a5547f0ee07aa5182036f148abc74
Tech-sis <abilawonadeola@gmail.com>
2022-03-28 07:38:51 -0700

    copy memory byte

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy - copy memory byte
+ * @dest: pointer to desctination
+ * @src: pointer to source
+ * Return: copy of memory byte
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
