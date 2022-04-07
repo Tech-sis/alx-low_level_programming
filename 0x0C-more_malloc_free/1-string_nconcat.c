c274ec2cac3eb93ceb9450118599b217d624af3a
Tech-sis <abilawonadeola@gmail.com>
2022-04-07 06:18:47 -0700

    concate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
+ * string_noncat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
  * @n: number of bytes to copy from string 2
@@ -9,22 +9,22 @@
  */
 char *string_noncat(char *s1, char *s2, unsigned int n)
 {
-    char *s3;
-    unsigned int i, j;
+	char *s3;
+	unsigned int i, j;
 
-    s3 = malloc(sizeof(char) * (strlen(s1) + n + 1));
-    if (s3 == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        s3[i] = s1[i];
-    }
-    for (j = 0; j < n; j++)
-    {
-        s3[i + j] = s2[j];
-    }
-    s3[i + j] = '\0';
-    return (s3);
+	s3 = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s3[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		s3[i + j] = s2[j];
+	}
+	s3[i + j] = '\0';
+	return (s3);
 }
