046cc63b7792a6b0d4b3bd875e66e011a4e0e8d4
Tech-sis <abilawonadeola@gmail.com>
2022-04-07 09:39:34 -0700

    concate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,28 +8,29 @@
  */
 char *string_noncat(char *s1, char *s2, unsigned int n)
 {
-    char *s3;
-    unsigned int i = 0, j = 0, k;
+	char *s3;
+	unsigned int i = 0, j = 0, k;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[i] != '\0')
-        i++;
-    while (s2[j] != '\0')
-        j++;
-    if (n > j)
-        n = j;
-    s3 = malloc(i + n + 1) * sizeof(char);
-    if (s3 == NULL)
-        return (0);
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
 
-    for (k = 0; k < i; k++)
-        s3[k] = s1[k];
-    for (; k < (i + n); k++)
-        s3[k] = s2[k - i];
-    s3[k] = '\0';
-    return (s3);
+	if (n > j)
+		n = j;
+	s3 = malloc(i + n + 1) * sizeof(char);
+	if (s3 == NULL)
+		return (0);
+
+	for (k = 0; k < i; k++)
+		s3[k] = s1[k];
+	for (; k < (i + n); k++)
+		s3[k] = s2[k - i];
+	s3[k] = '\0';
+	return (s3);
 }
