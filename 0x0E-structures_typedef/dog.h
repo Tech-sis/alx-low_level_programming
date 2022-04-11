3fc4124ecc5c122210abdf619061a37cc90daa2a
Tech-sis <abilawonadeola@gmail.com>
2022-04-11 11:01:17 -0700

    add dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,17 @@
 #define _DOG_H
 
 /**
- * struct dog  structure for dogs
+ * struct dog - structure for dogs
  * @name: name of dog
  * @age: age of dog
  * @owner: dog owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} dog_t;
+
 #endif
