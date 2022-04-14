9c84dabc340b4633a8429486fac9452e5fa98a59
Tech-sis <abilawonadeola@gmail.com>
2022-04-14 08:05:25 -0700

    add files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,6 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	int i;
+
 	i = 0;
 
 	while (ops[i].op)
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 	char o;
 
 	int (*func)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
