ab19c63d8242accaeda72d60afd437303b7368d5
Tech-sis <abilawonadeola@gmail.com>
2022-04-22 00:43:48 +0100

    print lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef _LISTS_H_
-#define _LIST_H_
+#ifndef _LISTS_
+#define _LISTS_
 
-size_t print_list(const list_t *h);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,5 +20,11 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
