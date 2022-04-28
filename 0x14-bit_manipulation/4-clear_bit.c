a7ab0fff475d104279bf9d4589d5c426ddedf97f
Tech-sis <abilawonadeola@gmail.com>
2022-04-28 18:17:40 +0100

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= (sizeof(unsigned long int) * BYTE_LENGTH))
 		return (-1);
 
-	*n ^= (0 << index);
+	*n &= ~(1 << index);
 
 	return (1);
 }
