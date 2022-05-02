dd1ce647921e2636af53587bc81314bc12b14085
Tech-sis <abilawonadeola@gmail.com>
2022-05-02 22:55:31 +0100

    correct mistakes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - read a file
- * @filename: name of file to read
- * @letters: length of input count
- * Return: return the length of letters count
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t rcount, wcount;
-	char *buffer;
+	ssize_t nrd, nwr;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
 		return (0);
 
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-
-	if (wcount == -1 || rcount != wcount)
-		return (0);
-
-	free(buffer);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
-	return (wcount);
+	free(buf);
+
+	return (nwr);
 }
